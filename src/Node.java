61702de2016bd45d4e1ec1979c19ac90cab37355
Arseniy Garelyshev <ars.garelyshev@yandex.ru>
2018-11-19 14:47:22 +0500

    Inital commit

---
+++ src/Node.java
@@ -0,0 +1,6 @@
+public class Node {
+    char value;
+    boolean isRed;
+    Node leftNode;
+    Node rightNode;
+}
---
+++ src/Tree.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Tree {
+    public static ArrayList<Node> tree = new ArrayList<>();
+
+    int find(char value){
+        return 0;
+    }
+
+    int addNode(char value){
+        return 0;
+    }
+
+    int removeNode(char value){
+        return 0;
+    }
+
+    void rebalance(){}
+}
